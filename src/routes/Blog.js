48f5f5e5204e3e198251265b2ee644d5ff5508c9
Rollin Metzger <rollin.metzger@gmail.com>
2019-08-19 18:07:52 -0500

    Create blog presentational component

---
+++ src/routes/Blog.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Blog = props => {
+    return(
+        <div id="blog-body">
+            <div id="blog-title">Blog</div>
+            <div id="blog-info">
+                <p>
+                    Info on Blog
+                </p>
+            </div>
+            <div id="blog-nav">
+                link to blog
+            </div>
+        </div>
+    )
+}
