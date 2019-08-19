ca5d9d47839ee39bb70b7c08f4e40a10d533c782
Rollin Metzger <alcesalces@github.com>
2019-08-19 17:55:06 -0500

    Create About presentational component

---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = props => {
+    return(
+        <div id="about-body">
+            <div id="about-title">Rollin Metzger</div>
+            <div id="about-info">
+                <p>
+                    Info
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
