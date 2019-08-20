1c364b284bdfb7f476285e7b0d21da4475ba297b
Rollin Metzger <rollin.metzger@gmail.com>
2019-08-20 18:41:11 -0500

    Create mapping function for projects

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
 class ProjectsContainer extends Component {
+    constructor(){
+        this.renderProjects = this.renderProjects.bind(this)
+        this.state = {
+            projects: {1: 1, 2: 2, 3: 3, 4: 4}
+        }
+    }
+
+    renderProjects() {
+        return this.state.projects.map(
+            project => <Project name={project.key} link={project.value} />
+        )
+    }
 
     render() {
         return (
-            <div>
-                link to all projects hosted on Heroku
+            <div className="projects-body">
+                {this.renderProjects()}
             </div>
         )
     }
