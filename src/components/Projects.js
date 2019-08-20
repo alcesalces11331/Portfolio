3f226f754f87c373c5905b7a9754c3ed105bd16b
Rollin Metzger <rollin.metzger@gmail.com>
2019-08-20 18:18:53 -0500

    Create basic component for dynamic props management

---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ProjectsContainer from '../containers/ProjectsContainer';
+
+class Projects extends Component {
+
+    render() {
+        return (
+            <div>Dynamic component for ProjectsContainer</div>
+        )
+    }
+}
+
+export default Projects;
