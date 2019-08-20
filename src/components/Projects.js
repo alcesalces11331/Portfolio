0f8a64eaee2944c4cf23386697c8829899ea5405
Rollin Metzger <rollin.metzger@gmail.com>
2019-08-20 18:57:20 -0500

    Build Project dynamic rendered component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const Project = props => <div class="project-container">
-    <div>Dynamic component for ProjectsContainer</div>
+const Project = props => <div className="project-container-body" key={this.props.name}>
+    <div className="project-container-detail">
+        <p className="project-name" href={this.props.address} >{this.props.name}</p>
+        <p className="project-description">{this.props.description}</p>
+    </div>
 </div>
 
 export default Projects;
