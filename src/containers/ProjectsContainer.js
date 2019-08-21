d265d38b78ba322e875c19fb84a4c8d84e1f55d7
Rollin Metzger <rollin.metzger@gmail.com>
2019-08-20 19:05:00 -0500

    Sync variable names between container & component

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -4,13 +4,13 @@ class ProjectsContainer extends Component {
     constructor(){
         this.renderProjects = this.renderProjects.bind(this)
         this.state = {
-            projects: {1: 1, 2: 2, 3: 3, 4: 4}
+            projects: {1: ['address', 'description'], 2: 2, 3: 3, 4: 4}
         }
     }
 
     renderProjects() {
         return this.state.projects.map(
-            project => <Project name={project.key} link={project.value} />
+            project => <Project name={project.key} address={project.value[0]} description={project.value[1]} />
         )
     }
 
