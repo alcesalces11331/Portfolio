df134ca1f289195de4f4075f29b145d77ef71c34
Rollin Metzger <rollin.metzger@gmail.com>
2019-09-02 21:38:09 -0500

    Reorganize the dom.
    This commit reorganizes the structure of the html/jsx of the dom. This will allow
    the structure to flow more seamlessly and more intuitively. The photo of myself
    has been removed to be saved for use within the About section.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Footer from './components/Footer';
 import About from './routes/About';
 import ProjectsContainer from './containers/ProjectsContainer';
 import Blog from './routes/Blog';
-import me from './images/me.jpg';
 import lodgepoleForestB from './images/lodgepoleForestB.jpg';
 
 function App() {
@@ -19,27 +18,29 @@ function App() {
         <div id="App-body-main">
           <div id="home-background-image-main">
             <img src={lodgepoleForestB} alt="Wintry High Altidude Pines" />
-          </div>
-          <div className="body-main-inner">
-            <div className="personal-text-container">
-              <div>
-                <h1>
-                  Rollin Metzger
-                  <br />
-                  Full-Stack Engineer
-                </h1>
-                <p>I am a driven full-stack developer with an insatiable curiosity for code</p>
-                <p>
-                  <a href="/about">About Me</a>
-                </p>
-              </div>
-              <div className="personal-image-container">
+            <div className="body-main-inner">
+              <div className="personal-text-container">
                 <div>
-                  <img src={me} alt='Myself' />
+                  <h1>
+                    Rollin Metzger
+                    <br />
+                    Full-Stack Engineer
+                  </h1>
+                  <p>I am a driven full-stack developer with an insatiable curiosity for code</p>
+                  <p>
+                    <a href="/about">About Me</a>
+                  </p>
                 </div>
               </div>
             </div>
-            
+            <div className="personal-quote-container">
+              <div>
+                <p>All that is gold does not glitter</p><br />
+                <p>Not all those who wandder are lost;</p><br />
+                <p>The old that is strong does not wither,</p><br />
+                <p>Deep roots are not reached by the frost.</p>
+              </div>
+            </div>
           </div>
         </div>
         <Route path="/blog" exact component={Blog} />
