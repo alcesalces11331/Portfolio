065d4c834c528c77533770b457550c0fcacfd7d1
Rollin Metzger <rollin.metzger@gmail.com>
2019-09-04 20:06:24 -0500

    Fix footer placement issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Footer = props => {
     return(
-        <div id="footer" className="align-center">
-            <p>Rollin Metzger</p>
-            <p>rollin.metzger@gmail.com</p>
+        <div id="footer-container">
+            <div id="footer" className="align-center">
+                <p>Rollin Metzger</p>
+                <p>rollin.metzger@gmail.com</p>
+            </div>
         </div>
     )
 }
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const About = props => {
     return(
-        <div id="about-body">
-            <div className="personal-quote-container">
-                <div>
-                    <p>All that is gold does not glitter</p><br />
-                    <p>Not all those who wandder are lost;</p><br />
-                    <p>The old that is strong does not wither,</p><br />
-                    <p>Deep roots are not reached by the frost.</p>
+        <div id="App-body-main">
+            <div className="body-main-inner">
+                <div className="personal-quote-container">
+                    <div>
+                        <p>All that is gold does not glitter</p><br />
+                        <p>Not all those who wandder are lost;</p><br />
+                        <p>The old that is strong does not wither,</p><br />
+                        <p>Deep roots are not reached by the frost.</p>
+                    </div>
                 </div>
             </div>
         </div>
