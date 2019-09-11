78bdbba34456e28f99d168820f5bcd2cb97e680f
Rollin Metzger <rollin.metzger@gmail.com>
2019-09-10 20:43:32 -0500

    Add responsive elements to home page

--- src/components/Background-Image.js
+++ src/components/Background-Image.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
 import oroborosEnso from '../images/oroborosEnso.png';
+import ensoEcosystem from '../images/ensoEcosystem.jpg';
 
 
 const BackgroundImage = props => {
     
     return(
-        <div id="home-background-image-main">
-            <Image path={"/"} src={oroborosEnso} alt="oroboso enso" className="App-logo-spin App-logo" roundedCircle />
+        <div id="home-background-image-container">
+            <div id="home-background-image-oroborosEnso">
+                <Image path={"/"} src={oroborosEnso} alt="oroboso enso" className="App-logo-spin App-logo" roundedCircle />
+            </div>
+            <div id="home-background-image-ensoEcosystem">
+                <Image path={"/"} src={ensoEcosystem} alt="enso ecosystem" />
+            </div>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = props => {
     return(
         <div id="App-body-main">
             <BackgroundImage />
-            <div className="body-main-inner">
+            <div className="body-main-inner-fullName">
                 <div className="personal-text-container">
                     <div>
                         <h1>
