0374964ed1ccda2e0311763f282dd7fb86bb9b28
Rollin Metzger <rollin.metzger@gmail.com>
2019-09-17 21:13:19 -0500

    Refactor and clean up /about css

--- src/routes/About.js
+++ src/routes/About.js
@@ -4,14 +4,12 @@ const About = props => {
     return(
         <div id="about-main-container">
             <div className="about-body-inner-container">
-                <div className="about-main-inner">
-                    <div className="personal-quote-container">
-                        <div>
-                            <p>All that is gold does not glitter</p>
-                            <p>Not all those who wandder are lost;</p>
-                            <p>The old that is strong does not wither,</p>
-                            <p>Deep roots are not reached by the frost.</p>
-                        </div>
+                <div className="personal-quote-container">
+                    <div>
+                        <p>All that is gold does not glitter</p>
+                        <p>Not all those who wandder are lost;</p>
+                        <p>The old that is strong does not wither,</p>
+                        <p>Deep roots are not reached by the frost.</p>
                     </div>
                 </div>
             </div>
