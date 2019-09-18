2d16056c5dcf632ee0b256f8309894102ff1d8a5
Rollin Metzger <rollin.metzger@gmail.com>
2019-09-17 21:56:37 -0500

    Add container div for footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Footer = props => {
     return(
-        <div id="footer-container">
-            <div id="footer" className="align-center">
-                <p>Rollin Metzger</p>
-                <p>rollin.metzger@gmail.com</p>
+        <div id="footer-main-container">
+            <div className="footer-inner-container">
+                <div className="">
+                    <p>Rollin Metzger</p>
+                    <p>rollin.metzger@gmail.com</p>
+                </div>
             </div>
         </div>
     )
