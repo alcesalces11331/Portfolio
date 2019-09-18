655cfb8bcb5b75793f68dc1bd862fa8cf616e7a0
Rollin Metzger <rollin.metzger@gmail.com>
2019-09-17 20:40:56 -0500

    Remove use of imported component for /about background

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import BackgroundImage from '../components/Background-Image';
 
 const About = props => {
     return(
-        <div id="App-body-main">
-            <div id="about-background-image-main">
-                <BackgroundImage />
-            </div>
-            <div className="about-main-inner">
-                <div className="personal-quote-container">
-                    <div>
-                        <p>All that is gold does not glitter</p>
-                        <p>Not all those who wandder are lost;</p>
-                        <p>The old that is strong does not wither,</p>
-                        <p>Deep roots are not reached by the frost.</p>
+        <div id="about-main-container">
+            <div className="about-body-inner-container">
+                <div className="about-main-inner">
+                    <div className="personal-quote-container">
+                        <div>
+                            <p>All that is gold does not glitter</p>
+                            <p>Not all those who wandder are lost;</p>
+                            <p>The old that is strong does not wither,</p>
+                            <p>Deep roots are not reached by the frost.</p>
+                        </div>
                     </div>
                 </div>
             </div>
