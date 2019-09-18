be0a2b011ec5a76be9a9804e8e6b9e1d770c7857
Rollin Metzger <rollin.metzger@gmail.com>
2019-09-18 17:59:23 -0500

    Add necessary containers for footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,18 @@ const Footer = props => {
     return(
         <div id="footer-main-container">
             <div className="footer-inner-container">
-                <div className="">
-                    <p>Rollin Metzger</p>
-                    <p>rollin.metzger@gmail.com</p>
+                <div className="info-personal-container">
+                    <div className="info-personal">
+                        <p>Rollin Metzger</p>
+                        <p>rollin.metzger@gmail.com</p>
+                    </div>
+                </div>
+                <div className="social-media-link-container">
+                    <div className="social-media-link">
+                        <a href="#" className="fa fa-facebook"></a>
+                        <a href="#" className="fa fa-twitter"></a>
+                        <a href="#" className="fa fa-instagram"></a>
+                    </div>
                 </div>
             </div>
         </div>
