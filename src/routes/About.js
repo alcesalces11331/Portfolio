c48afc02cc51b60511ba7ef60fe2b5d5b796ec5c
Rollin Metzger <rollin.metzger@gmail.com>
2019-09-28 14:57:30 -0500

    Refactor app.css to reflect about.js page changes
    This commit adds some more text content to the about page. Also, it
    adds necessary css styling for the new container. Completely scalable.

--- src/routes/About.js
+++ src/routes/About.js
@@ -6,10 +6,11 @@ const About = props => {
         <div id="about-main-container">
             <div className="personal-quote-container ">
                 <div>
-                    <p>All that is gold does not glitter</p>
-                    <p>Not all those who wander are lost;</p>
-                    <p>The old that is strong does not wither,</p>
-                    <p>Deep roots are not reached by the frost.</p>
+                    <p>All that is gold does not glitter</p><br />
+                    <p>Not all those who wander are lost;</p><br />
+                    <p>The old that is strong does not wither,</p><br />
+                    <p>Deep roots are not reached by the frost.</p><br />
+                    <p>-J.R.R. Tolkien</p>
                 </div>
             </div>
             <div className="about-body-inner-container about-row-1">
@@ -25,21 +26,30 @@ const About = props => {
                 <div className="personal-image-container">
                     <ImageRollin />
                 </div>
-                <div className="about-text-bottom">
-                    <p>
-                        Languages I'm currently using:<br />
-                        React and Javascript<br />
-                        Ruby on Rails<br />
-                        SQL<br />
-                    </p>
-                    <p>
-                        Languages I'm currently learning/wanting to learn:<br />
-                        React Hooks<br />
-                        Groovy/Grails and Java<br />
-                        Python<br />
-                        MongoDB<br />
-                        Go<br />
-                    </p>
+                <div className="about-text-bottom-container">
+                    <div className="about-text-bottom-1">
+                        <p>
+                            Languages I'm currently using:<br />
+                            React and Javascript<br />
+                            Ruby on Rails<br />
+                            SQL<br />
+                        </p>
+                        <p>
+                            Languages I'm currently learning/wanting to learn:<br />
+                            React Hooks<br />
+                            Groovy/Grails and Java<br />
+                            Python<br />
+                            MongoDB<br />
+                            Go<br />
+                        </p>
+                    </div>
+                    <div className="about-text-bottom-2">
+                        <p>
+                            Projects I'm working on:<br />
+                            This one! Hoisting my projects and creating a personal, home-designed, blog.<br />
+                            A D&D adventure creator<br />
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
